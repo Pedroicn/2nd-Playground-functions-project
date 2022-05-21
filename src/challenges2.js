6a704139c290025b6066d9bdc031535d709eb51c
Pedro Nunes <pedro.icn88@gmail.com>
2022-05-21 14:43:20 -0300

    Desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,8 +1,28 @@
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(array) {
   // seu código aqui
+  let phoneNumber = '(' + array[0] + array[1] + ')' + ' ' + array[2] + array[3] + array[4] + array[5] + array[6] + '-' + array[7] + array[8] + array[9] + array[10];
+  if(array.length !== 11){
+    return 'Array com tamanho incorreto.';
+  }
+  for(let i = 0; i < array.length; i += 1){
+    if(array[i] < 0 || array[i] > 9){
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+    let contador = 0;
+    for(let j = 0; j < array.length; j += 1){
+      if(array[i] === array[j]){
+        contador += 1;
+      }
+ 
+    }
+    if(contador >= 3){
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  return phoneNumber;
 }
-
+// console.log(generatePhoneNumber([2, 3, 3, 6, 5, 6, 7, 8, 9, 0, 1]))
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
